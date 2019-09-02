48888111a08fa994de6fafe2d56cbb09640d620e
straf16 <depoksugutamu621@gmail.com>
2019-09-02 20:58:26 +0700

    mengerjakan number in words release 2

--- number_in_words.js
+++ number_in_words.js
@@ -1,55 +1,96 @@
 function numberToWords(number) {
   // Your code here
   var str = '';
-  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var words = ['nol', 'satu ', 'dua ', 'tiga ', 'empat ', 'lima ', 'enam ', 'tujuh ', 'delapan ', 'sembilan '];
-  var satuan = ['','puluh ', 'ratus ', 'ribu '];
-  var x = 0;
-  while (number > 0) {
-    if (str) {
-      words[1] = 'se';
-    }
-    for (var i = 0; i < numbers.length; i++) {
-      if (number % 10 == 0) {
-        number = Math.floor((number/10));
-        x++;
-        if (x == satuan.length) {
-          x = 0;
-        }
-        break;
-      }
-      if (number % 10 == numbers[i]) {
-        var temp = str;
-        if (x == 1 && words[i] == 'se') {
-          if (temp == 'satu ') {
-            temp = 'se';
-          }
-          str = temp + 'belas'
-        } else {
-          str = words[i] + satuan[x] + temp;
-        }
-        number = Math.floor((number/10));
-        x++;
-        if (x == satuan.length) {
-          x = 1;
-        }
-        break;
-      }
-    }
+  var words = {
+    11: 'sebelas',
+    10: 'sepuluh',
+    9: 'sembilan',
+    8: 'delapan',
+    7: 'tujuh',
+    6: 'enam',
+    5: 'lima',
+    4: 'empat',
+    3: 'tiga',
+    2: 'dua',
+    1: 'satu',
+    0: 'nol',
   }
-  
+  var satuan = ['belas', 'puluh', 'ratus', 'ribu', 'juta', 'milyar', 'triliun'];
+  var rangeSatuan = [20,      99,     999, 999999, 999999999, 999999999999, 999999999999999];
+
+  if (isNaN(number) || number == 0) {
+    return '';
+  } else if (words[number]) {
+    return words[number];
+  }
+  for (var i = 0; i < rangeSatuan.length; i++) {
+    if (number <= rangeSatuan[i]) {
+      if (i == 0) {
+        str += numberToWords(number%10) + ' ' + satuan[i];
+      } else if (i == 1) {
+        if (number == 10) {
+          str += 'sepuluh';
+        } else {
+          str += numberToWords(Math.floor(number/Math.pow(10, i))) + ' ' + satuan[i] + ' ' + numberToWords((number%Math.pow(10, i)));
+        }
+      } else if (i == 2) {
+        if (number <= 199) {
+          str += 'se' + satuan[i] + ' ' + numberToWords((number%Math.pow(10, i)));
+        } else {
+          str += numberToWords(Math.floor(number/Math.pow(10, i))) + ' ' + satuan[i] + ' ' + numberToWords((number%Math.pow(10, i)));
+        }
+      } else if (i == 3) {
+        if (number <= 1999) {
+          str += 'se' + satuan[i] + ' ' + numberToWords((number%Math.pow(10, i)));
+        } else {
+          str += numberToWords(Math.floor(number/Math.pow(10, i))) + ' ' + satuan[i] + ' ' + numberToWords((number%Math.pow(10, i)));
+        }
+      } else if (i == 4) {
+        str += numberToWords(Math.floor(number/Math.pow(10, 6))) + ' ' + satuan[i] + ' ' + numberToWords((number%Math.pow(10, 6)));
+      } else if (i == 5) {
+        str += numberToWords(Math.floor(number/Math.pow(10, 9))) + ' ' + satuan[i] + ' ' + numberToWords((number%Math.pow(10, 9)));
+      } else if (i == 6) {
+        str += numberToWords(Math.floor(number/Math.pow(10, 12))) + ' ' + satuan[i] + ' ' + numberToWords((number%Math.pow(10, 12)));
+      }
+      break;
+    }
+    
+  }
   return str;
+
+
+  // 1223
+  // numberToWords(1000) + sisa
+  // dua ratus + numberToWords(23)
+  // tiga ribu seratus dua puluh tiga
+  // return numberToWords(8) + 'belas'
+  // return numberToWords(2) + 'puluh' + numberToWords()
+
+  
+  // return hasil;
 }
 
 // Driver code
 
 console.log(numberToWords(4));
+console.log(numberToWords(11));
+console.log(numberToWords(13));
 console.log(numberToWords(27));
-console.log(numberToWords(102));
-console.log(numberToWords(38079));
-// console.log(numberToWords(82102713));
-
+console.log(numberToWords(111));
+console.log(numberToWords(18079));
+console.log(numberToWords(82102713));
 
 console.log(numberToWords(705));
-// console.log(numberToWords(1000000));
-// console.log(numberToWords(2011845));
+console.log(numberToWords(1000000));
+console.log(numberToWords(2011845));
+
+console.log('');
+
+console.log(numberToWords(1));
+console.log(numberToWords(11));
+console.log(numberToWords(111));
+console.log(numberToWords(1111));
+console.log(numberToWords(11111));
+console.log(numberToWords(111111));
+console.log(numberToWords(1111111));
+console.log(numberToWords(111111111111111));
