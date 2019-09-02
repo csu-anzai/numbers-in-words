f2534b8b9bbe9321e096294800df061459c95c9a
Kevin Anantha <kevin.anantha@gmail.com>
2019-09-02 20:30:34 +0700

    :anchor:

--- number_in_words.js
+++ number_in_words.js
@@ -1,8 +1,57 @@
 function numberToWords(number) {
-  // Your code here
+  const kamus = [ 'nol', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas' ]
+  let words = ''
+
+  number = Math.floor(number)
+
+  if (number < 12) {
+    return kamus[number]
+  } else if (number < 20) {
+    return numberToWords(number % 10) + ' belas'
+  } else if (number < 100) {
+    if (number % 10 == 0) {
+      return numberToWords(number / 10) + ' puluh'
+    }
+    return numberToWords(number / 10) + ' puluh ' + numberToWords(number % 10)
+  } else if (number < 200) {
+    return 'seratus ' + numberToWords(number % 100)
+  } else if (number < 1000) {
+    if (number % 100 == 0) {
+      return numberToWords(number / 100) + ' ratus'
+    }
+      return numberToWords(number / 100) + ' ratus ' + numberToWords(number % 100)
+  } else if (number < 2000) {
+    if (number == 1000) return 'seribu'
+    return 'seribu ' + numberToWords(number % 1000)
+  } else if (number < 1000000) {
+    if (number % 1000 == 0) {
+      return numberToWords(number / 100) + ' ribu'
+    }
+    return numberToWords(number / 1000) + ' ribu ' + numberToWords(number % 1000)
+  } else if (number < 1000000000) {
+    if (number % 1000000 == 0) {
+      return numberToWords(number / 1000000) + ' juta'
+    }
+    return numberToWords(number / 1000000) + ' juta ' + numberToWords(number % 1000000)
+  } else if (number < 1000000000000) {
+    if (number % 1000000000 == 0) {
+      return numberToWords(number / 1000000000) + ' milyar'
+    }
+    return numberToWords(number / 1000000000) + ' milyar ' + numberToWords(number % 1000000000)
+  } else if (number < 1000000000000000) {
+    if (num % 1000000000000 === 0) {
+      return numberToWords(number / 1000000000000) + ' triliun'
+    }
+    return numberToWords(number / 1000000000000) + ' triliun ' + numberToWords(num % 1000000000000)
+  }
 }
 
 // Driver code
 console.log(numberToWords(705));
+console.log(numberToWords(1001));
+console.log(numberToWords(1000));
+console.log(numberToWords(111));
+console.log(numberToWords(1111));
+console.log(numberToWords(11111));
 console.log(numberToWords(1000000));
 console.log(numberToWords(2011845));
